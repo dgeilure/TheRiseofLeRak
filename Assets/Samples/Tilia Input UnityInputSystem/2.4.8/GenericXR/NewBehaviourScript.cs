2e24cbba6dd5fdc0d693f2c8cf8542e7c5a1d2a5
Daniela Geilure <geilureda88424@th-nuernberg.de>
2023-11-18 15:33:22 +0100

    Added reaction to gesture recognition

--- Assets/Samples/Tilia Input UnityInputSystem/2.4.8/GenericXR/NewBehaviourScript.cs
+++ Assets/Samples/Tilia Input UnityInputSystem/2.4.8/GenericXR/NewBehaviourScript.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject table;
+    private Material tableMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tableMaterial = table.GetComponent<Renderer>().material;
+        tableMaterial.SetColor("_Color", Color.white);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnGestureCompleted(GestureCompletionData gestureCompletionData)
     {
         if (gestureCompletionData.gestureID < 0)
         {
             string errorMessage = GestureRecognition.getErrorMessage(gestureCompletionData.gestureID);
-
+            Debug.Log("Gesture not recognized " + errorMessage);
+            tableMaterial.SetColor("_Color", Color.red);
             return;
         }
-        if (gestureCompletionData.similarity >= 0.5) { }
+
+        if (gestureCompletionData.similarity >= 0.5) 
+        {
+            tableMaterial.SetColor("_Color", Color.green);
+        }
     }
 }
